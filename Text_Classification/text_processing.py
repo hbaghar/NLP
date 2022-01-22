609f2dd147d628463433c1be324c21646548a681
Hriday Baghar <hbaghar@uw.edu>
2022-01-21 17:32:26 -0800

    Disabled tf() run on init and made methods private

--- Text_Classification/text_processing.py
+++ Text_Classification/text_processing.py
@@ -7,15 +7,14 @@ class Document():
     def __init__(self, filepath):
         self.__filepath = filepath
         self.sentiment = filepath[:filepath.rfind('/')][-3:]
-        self.__tokens = self.process_file()
-        self.frequency_dict = self.tf()
+        self.__tokens = self.__process_file()
         self.num_words = self.get_num_words()
 
     def __str__(self):
         end = self.__filepath.rfind('/')
         return f"File name: %s (%s) (%d words)" % (self.__filepath[end+1:], self.sentiment, self.num_words)
 
-    def process_file(self):
+    def __process_file(self):
         """
         Perform text processing on a raw txt file given its filepath
         Returns a list of tokenized words after removing special characters
@@ -27,7 +26,7 @@ class Document():
         text = re.sub('[^a-zA-Z\d\s]+', "", text)
         return [token for token in text.split(' ') if token != '']
 
-    def word_count(self, token):
+    def __word_count(self, token):
         """
         Counts occurences of a specific word
         """
@@ -36,8 +35,8 @@ class Document():
     def get_num_words(self):
         return len(self.__tokens)
 
-    def tf(self):
+    def __tf(self):
         word_dict = {}
         for token in set(self.__tokens):
-            word_dict[token] = self.word_count(token)/len(self.__tokens)
+            word_dict[token] = self.__word_count(token)/len(self.__tokens)
         return word_dict
