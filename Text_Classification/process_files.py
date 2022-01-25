14fa9a556f322ee7eb3a03f736fb624bb6a81341
Hriday Baghar <hbaghar@uw.edu>
2022-01-25 02:47:14 -0800

    Created function for dataset creation using tf-idf scores

--- Text_Classification/process_files.py
+++ Text_Classification/process_files.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm
 from glob import glob
 from text_processing import Document
+import numpy as np
 
 class ProcessDocuments(object):
     """docstring for ProcessDocuments."""
@@ -9,7 +10,9 @@ class ProcessDocuments(object):
     def __init__(self, root='Text_Classification/review_polarity'):
         self.__root = root
         self.__documents = []
-        self.vocab = {}
+        self.__word_doc_freq = {}
+        self.vocab = None
+        self.data = None
 
     def __get_filepaths(self):
         """
@@ -27,17 +30,26 @@ class ProcessDocuments(object):
         """
         Function that will gather all the words from each document and build a bag of words
         """
-        for doc in tqdm(self.__documents):
+        for doc in tqdm(self.__documents, desc = "Generating vocabulary"):
+            for token in doc.tf().keys():
+                self.__word_doc_freq[token] = self.__word_doc_freq.get(token, 0) + 1
 
+        self.vocab = {token: i for token,i in zip(self.__word_doc_freq.keys(), range(len(self.__word_doc_freq)))}
 
-    def run(self):
+    def get_dataset(self):
         self.__get_documents()
         self.__get_vocab()
 
+        self.data = np.zeros((len(self.__documents), len(self.__word_doc_freq)+1))
+        for i,doc in enumerate(tqdm(self.__documents, desc = "Craeating dataset")):
+            for token, val in doc.tf().items():
+                idx = self.vocab[token]
+                self.data[i,idx] = val*np.log(len(self.__documents)/(self.__word_doc_freq[token]+1))
+                self.data[i,-1] = 1 if doc.sentiment == "pos" else -1
 
 def main():
     processor = ProcessDocuments()
-    processor.run()
+    processor.get_dataset()
 
 if __name__ == "__main__":
     main()
