e7dd460a900fe1047d379f555a2822270747a1c0
Hriday Baghar <hbaghar@uw.edu>
2022-01-25 00:22:56 -0800

    Refactored as a class

--- Text_Classification/process_files.py
+++ Text_Classification/process_files.py
@@ -3,26 +3,41 @@ from tqdm import tqdm
 from glob import glob
 from text_processing import Document
 
-def get_filepaths(root):
-    """
-    Return file path for all txt files listed in a directory and sub-directories
-    """
-    return [file for dir in os.walk(root) for file in glob(os.path.join(dir[0], '*.txt'))]
+class ProcessDocuments(object):
+    """docstring for ProcessDocuments."""
 
-#Convert to input later
+    def __init__(self, root='Text_Classification/review_polarity'):
+        self.__root = root
+        self.__documents = []
+        self.vocab = {}
+
+    def __get_filepaths(self):
+        """
+        Return file path for all txt files listed in a directory and sub-directories
+        """
+        return [file for dir in os.walk(self.__root) for file in glob(os.path.join(dir[0], '*.txt'))]
+
+    def __get_documents(self):
+        """
+        Create a list of document objects for each text file
+        """
+        self.__documents = [Document(file) for file in tqdm(self.__get_filepaths(), desc = "Processing documents")]
+
+    def __get_vocab(self):
+        """
+        Function that will gather all the words from each document and build a bag of words
+        """
+        for doc in tqdm(self.__documents):
+
+
+    def run(self):
+        self.__get_documents()
+        self.__get_vocab()
+
+
 def main():
-    path = 'Text_Classification/review_polarity'
-
-    #We need to process all files and create a vocabulary
-    dir = os.path.join(os.curdir, path)
-    vocab = set()
-    for file in tqdm(get_filepaths(dir), desc = "Processing files"):
-        doc = Document(file)
-        vocab.update(set(doc.tokens))
-
-    with open('Text_Classification/vocabulary.txt', 'w+') as f:
-        for word in tqdm(vocab, desc = "Creating vocabulary"):
-            f.write(word+"\n")
+    processor = ProcessDocuments()
+    processor.run()
 
 if __name__ == "__main__":
     main()
