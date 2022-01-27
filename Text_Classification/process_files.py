634fd56efa93591313ff24f54616e4837afafc00
Hriday Baghar <hbaghar@uw.edu>
2022-01-27 13:54:34 -0800

    Updated docstring for class, removed redundant method creating list of Document() objects

--- Text_Classification/process_files.py
+++ Text_Classification/process_files.py
@@ -4,14 +4,29 @@ from glob import glob
 from text_processing import Document
 import numpy as np
 
-class ProcessDocuments(object):
-    """docstring for ProcessDocuments."""
-    #Remove root and pass location as parameter for each funtion
+class ProcessFiles(object):
+    """
+    Creates training and test data for a folder containing raw txt files.
+
+    Text files should be housed in a folder structure as follows:
+
+    .
+    |-data
+    |    |_ train
+    |    |   |_ pos
+    |    |   |_ neg
+    |    |_ test
+    |        |_ pos
+    |        |_ neg
+    |_ process_files.py
+    |_ text_processing.py
+    """
+
     def __init__(self, train_path = 'Text_Classification/data/train', test_path = 'Text_Classification/data/test'):
         self.train_path = train_path
         self.test_path = test_path
-        self.__train_documents = []
-        self.__test_documents = []
+        self.__train_files = []
+        self.__test_files = []
         self.__word_doc_freq = {}
         self.vocab = None
         self.train_data = None
@@ -22,41 +37,35 @@ class ProcessDocuments(object):
         Return file path for all txt files listed in a directory and sub-directories
         """
         return [file for dir in os.walk(path) for file in glob(os.path.join(dir[0], '*.txt'))]
-    #Return  value and call separately for train and test data
-    def __get_documents(self, path):
-        """
-        Create a list of document objects for each text file
-        """
-        return [Document(file) for file in tqdm(self.__get_filepaths(path), desc = "Processing documents")]
 
-    #Call when training data is being created
     def __get_vocab(self):
         """
         Function that will gather all the words from each document and build a bag of words
         """
-        for doc in tqdm(self.__train_documents, desc = "Generating vocabulary"):
+        for file in tqdm(self.__train_files, desc = "Generating vocabulary"):
+            doc = Document(file)
             for token in doc.tf().keys():
                 self.__word_doc_freq[token] = self.__word_doc_freq.get(token, 0) + 1
 
         #Creating lookup table using dictionary for faster search for idx in get_dataset
         self.vocab = {token: i for token,i in zip(self.__word_doc_freq.keys(), range(len(self.__word_doc_freq)))}
 
-    def __get_data_matrix(self, docs):
+    def __get_data_matrix(self, files):
         """
         Creates dataset in the form of a numpy array. Features are tf-idf scores and last column is sentiment label
         """
         #Define matrix size based on document and feature counts
-        x = np.zeros((len(docs), len(self.__word_doc_freq)))
-        y = np.zeros((len(docs), 1))
-        #Split into function and allow passing custom list of document objects (so that it can be re-run for train and test)
-        #Iterating every document
-        for i,doc in enumerate(tqdm(docs, desc = "Creating dataset")):
+        x = np.zeros((len(files), len(self.__word_doc_freq)))
+        y = np.zeros((len(files), 1))
+        #Iterating over every document
+        for i, file in enumerate(tqdm(files, desc = "Creating dataset")):
             #Iterating every word in document
+            doc = Document(file)
             for token, val in doc.tf().items():
                 try:
                     idx = self.vocab[token]
                     #Calculating tf-idf score
-                    x[i,idx] = val*np.log(len(docs)/(self.__word_doc_freq[token]+1))
+                    x[i,idx] = val*np.log(len(files)/(self.__word_doc_freq[token]+1))
                 except:
                     pass
                 #Assigning labels
@@ -66,19 +75,19 @@ class ProcessDocuments(object):
 
     def get_train_data(self):
         print("Processing training data files...")
-        self.__train_documents = self.__get_documents(self.train_path)
+        self.__train_files = self.__get_filepaths(self.train_path)
         self.__get_vocab()
 
         print("\nCreating training dataset...")
-        self.train_data = self.__get_data_matrix(self.__train_documents)
+        self.train_data = self.__get_data_matrix(self.__train_files)
 
         return self.train_data
 
     def get_test_data(self):
         print("\nProcessing test data files...")
-        self.__test_documents = self.__get_documents(self.test_path)
+        self.__test_files = self.__get_filepaths(self.test_path)
 
-        print("\nCreating test dataset...")
-        self.test_data = self.__get_data_matrix(self.__test_documents)
+        print("Creating test dataset...")
+        self.test_data = self.__get_data_matrix(self.__test_files)
 
         return self.test_data
